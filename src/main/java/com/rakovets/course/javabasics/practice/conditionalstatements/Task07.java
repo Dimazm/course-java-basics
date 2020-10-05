f1a648e1e5c6ff1db2df9e8a20539e3e5110f2fa
dimazm@live.com <Pvfxbycrbq1981>
2020-10-05 18:35:05 +0300

    Task07 Done

--- src/main/java/com/rakovets/course/javabasics/practice/conditionalstatements/Task07.java
+++ src/main/java/com/rakovets/course/javabasics/practice/conditionalstatements/Task07.java
@@ -21,9 +21,20 @@ public class Task07 {
         // Код необходимый для тестирования, не изменять
         Scanner scanner = new Scanner(System.in);
         int year = (args.length != 1) ? scanner.nextInt() : Integer.parseInt(args[0]);
+        if (isleapYear(year)) {
+            System.out.println("leap year");
+        } else
+            System.out.println("not a leap year");
+    }
 
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
-        // Для проверки решения необходимо запустить @Test для данного class (в директории test)
+    static boolean isleapYear(int year) {
+        if (year % 4 == 0 && year % 400 !=0)
+       {
+            return true;
+        } else if (year % 4 != 0 && year % 400 ==0) {
+            return false;
+        }
+        return false;
+
     }
 }
