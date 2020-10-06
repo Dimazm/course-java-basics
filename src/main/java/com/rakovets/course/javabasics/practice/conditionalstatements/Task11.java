f95968677fc006dfdc908d6b45b9036d76dab101
dimazm@live.com <Pvfxbycrbq1981>
2020-10-06 17:54:49 +0300

    Task11 done

--- src/main/java/com/rakovets/course/javabasics/practice/conditionalstatements/Task11.java
+++ src/main/java/com/rakovets/course/javabasics/practice/conditionalstatements/Task11.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
  * - спец. символ
  * Так же программа не должна зависеть от регистра. Известно:
  *
- * @param letter - символ введенный пользователем
+ * @param // letter - символ введенный пользователем
  * @return 'latin'/'cyrillic'/'digit'/'undefined'
  * <p>
  * Help: Unicode (decimal)
@@ -25,9 +25,20 @@ public class Task11 {
         // Код необходимый для тестирования, не изменять
         Scanner scanner = new Scanner(System.in);
         char letter = (args.length != 1) ? scanner.next().charAt(0) : args[0].charAt(0);
-
+        int letterCode = letter;
+        //System.out.println("Input русс- " + letter);
         //TODO
         // Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
         // Для проверки решения необходимо запустить @Test для данного class (в директории test)
+        if (letterCode >= 65 && letterCode <= 90 || letterCode >= 97 && letterCode <= 122) {
+            System.out.println("latin");
+        } else if
+        (letterCode >= 176 && letterCode <= 239) {
+            System.out.println("cyrillic");
+        } else if
+        (letterCode >= 48 && letterCode <= 57) {
+            System.out.println("digit");
+        } else System.out.println("undefined");
+
     }
 }
